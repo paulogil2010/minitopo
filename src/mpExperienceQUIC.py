2a574319622a6a7abad75a19e9d6b88d76e21c1a
Paulo Lenz Junior <paulogil2010@gmail.com>
2020-05-07 00:06:08 -0300

    add satanas

--- src/mpExperienceQUIC.py
+++ src/mpExperienceQUIC.py
@@ -64,7 +64,7 @@ class MpExperienceQUIC(MpExperience):
 		if int(self.multipath) <= 0:
 			s = "./main"
 		else:
-			s = "sh /home/mininet/git/minitopo/src/mpShoutdownNic.sh; ./main"
+			s = "sh /home/mininet/git/minitopo/src/mpShoutdownNic.sh & ./main"
 			s += " -m"
 		s += " -c https://" + self.mpConfig.getServerIP() + ":6121/random &>" + MpExperienceQUIC.CLIENT_LOG
 		return s
